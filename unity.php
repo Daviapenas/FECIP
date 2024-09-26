f5714d2bddecd56c2d63cd3182c8164385289d4d
Daviapenas <106933113+Daviapenas@users.noreply.github.com>
2024-09-26 19:38:29 -0300

    TESTE AI
    TESTEEEE

--- unity.php
+++ unity.php
@@ -31,7 +31,7 @@ echo "Caiu no catch: " . $e->getMessage();
     $response = array('status' => 'erro', 'message' => $e->getMessage());
     echo json_encode($response);
 } finally {
-    // Fechar a conexão, se estiver aberta
+    // Fechar a conexão, se estiver aberta AAAAAAAAAAAAAAAA TESTE
     if (isset($conn) && $conn !== false) {
         sqlsrv_close($conn);
     }
